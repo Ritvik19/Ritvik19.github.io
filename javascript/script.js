a9927701c9fe10dcf3b9452fac3edda71ef8f274
Ritvik19 <rastogiritvik99@gmail.com>
2018-07-29 15:00:26 +0530

    Animationsgit add .

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
   $(".block").hide();
   $("#home").show(5000);
 });
+// alert("linked");
+$("#12").click(function(){
+  $("#picture, #header, #intro, #links, #dwnld").slideUp(5000, function(){
+      $("#inavigate").hide();
+      $("#home").hide();
+      $("#skills").show();
+      $("#skill-content").hide();
+      $("#snavigate").show();
+      $("#skill-content").show(5000);
+  });
+});
