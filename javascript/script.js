46380626c265ea6990a7714e68b3a1426ed5c97a
Ritvik19 <rastogiritvik99@gmail.com>
2018-07-29 17:14:17 +0530

    Animations Fixed

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,23 @@ $(document).ready(function(){
   $("#home").show(5000);
 });
 // alert("linked");
-$("#12").click(function(){
-  $("#picture, #header, #intro, #links, #dwnld").slideUp(5000, function(){
+$("#13").click(function(){
+  $("#picture, #header, #intro, #links, #dwnld").hide(5000, function(){
       $("#inavigate").hide();
       $("#home").hide();
       $("#skills").show();
       $("#skill-content").hide();
       $("#snavigate").show();
       $("#skill-content").show(5000);
+  });
+});
+$("#31").click(function(){
+  $("#skill-content").hide(5000, function(){
+      $("#snavigate").hide();
+      $("#skills").hide();
+      $("#home").show();
+      $("#picture, #header, #intro, #links, #dwnld").hide();
+      $("#inavigate").show();
+      $("#picture, #header, #intro, #links, #dwnld").show(5000);
   });
 });
