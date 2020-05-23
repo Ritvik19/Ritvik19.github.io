3d9997097b97f48020f62ebbe394872a22f4c3a7
Ritvik19 <rastogiritvik99@gmail.com>
2020-05-23 11:56:46 +0530

    Minor Changes

--- js/loadcontents.js
+++ js/loadcontents.js
@@ -61,12 +61,12 @@ var ctx1 = document.getElementById('chart1').getContext('2d');
 var chart1 = new Chart(ctx1, {
     type: 'radar',
     data: {
-        labels: ['HTML/CSS', 'JavaScript', 'Flask'],
+        labels: ['HTML', 'CSS', 'JavaScript', 'CSS Frameworks', 'Flask'],
         datasets: [{
             label: 'Web Development',
             backgroundColor: 'rgba(89, 24, 223, 0.5)',
             borderColor: 'rgb(89, 24, 223)',
-            data: [90, 80, 70]
+            data: [90, 90, 85, 85, 75]
         }]
     },
     options: chart_options,
@@ -91,12 +91,12 @@ var ctx3 = document.getElementById('chart3').getContext('2d');
 var chart3 = new Chart(ctx3, {
     type: 'radar',
     data: {
-        labels: ['Java (Core)', 'C++', 'Bash Scripting and Shell Programming'],
+        labels: ['Java (Core)', 'C++', 'Bash Scripting', 'C', 'DS Algo'],
         datasets: [{
             label: 'Other',
             backgroundColor: 'rgba(89, 24, 223, 0.5)',
             borderColor: 'rgb(89, 24, 223)',
-            data: [70, 70, 80]
+            data: [70, 70, 80, 75, 70]
         }]
     },
     options: chart_options,
