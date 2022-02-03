bdc13964cd88abc3942e49302e73d2fe2da78bbd
Ritvik19 <rastogiritvik99@gmail.com>
2022-02-03 22:59:34 +0530

    pyradox-tabular

--- assets/data/data.js
+++ assets/data/data.js
@@ -1,34 +1,46 @@
-const data = [{
-        title: "pyradox",
-        description: "A library that helps you with implementing various state of the art neural networks in a totally customizable fashion using TensorFlow 2",
-    },
-    {
-        title: "pyradox-generative",
-        description: "A library that provides Light Weight Trainers For The State Of The Art Generative Neural Networks Using A TensorFlow Backend",
-    },
-    {
-        title: "text-data-augmentation",
-        description: "A library that implements various State of the Art Text Data Augmentation Techniques for Natural Language Processing Applications",
-    },
-    {
-        title: "vizard",
-        description: "A library that provides you with Low Code Data Visualisations for any Tabular Data Science Project",
-    },
-    {
-        title: "ai-sudoku-solver",
-        description: "A library that helps Solving Sudoku Puzzles using Artificial Neural Networks",
-    },
-    {
-        title: "implemented-data-science",
-        description: "This Library of Implementations of various data science techniques and research papers",
-    },
+const data = [
+  {
+    title: "pyradox",
+    description:
+      "A library that helps you with implementing various state of the art neural networks in a totally customizable fashion using TensorFlow 2",
+  },
+  {
+    title: "pyradox-generative",
+    description:
+      "A library that provides Light Weight Trainers For The State Of The Art Generative Neural Networks Using A TensorFlow Backend",
+  },
+  {
+    title: "pyradox-tabular",
+    description:
+      "A library that helps you train various state of the art neural networks for tabular data using TensorFlow 2",
+  },
+  {
+    title: "text-data-augmentation",
+    description:
+      "A library that implements various State of the Art Text Data Augmentation Techniques for Natural Language Processing Applications",
+  },
+  {
+    title: "vizard",
+    description:
+      "A library that provides you with Low Code Data Visualisations for any Tabular Data Science Project",
+  },
+  {
+    title: "ai-sudoku-solver",
+    description:
+      "A library that helps Solving Sudoku Puzzles using Artificial Neural Networks",
+  },
+  {
+    title: "implemented-data-science",
+    description:
+      "This Library of Implementations of various data science techniques and research papers",
+  },
 ];
 
 const skills = {
-    "Python-Programming": 95,
-    "Machine-Learning": 90,
-    "Deep-Learning": 85,
-    "Natural-Language-Processing": 85,
-    "Computer-Vision": 80,
-    "Web-Scraping": 90,
+  "Python-Programming": 95,
+  "Machine-Learning": 90,
+  "Deep-Learning": 85,
+  "Natural-Language-Processing": 85,
+  "Computer-Vision": 80,
+  "Web-Scraping": 90,
 };
