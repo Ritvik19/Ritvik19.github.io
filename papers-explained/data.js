cb108307df25aa7633e33ae2b8da5998f27c4500
Ritvik Rastogi <36080978+Ritvik19@users.noreply.github.com>
2023-12-29 09:21:21 +0530

    Update data.js

--- papers-explained/data.js
+++ papers-explained/data.js
@@ -533,6 +533,12 @@ const papers_data = [
         "Uses a compound scaling method to scale the network's depth, width, and resolution to achieve a high accuracy with a relatively low computational cost.",
     },
     {
+      title: "NF Net",
+      link: "https://ritvik19.medium.com/papers-explained-84-nf-net-b8efa03d6b26",
+      date: "February 2021",
+      description: "An improved class of Normalizer-Free ResNets that implement batch-normalized networks, offer faster training times, and introduce an adaptive gradient clipping technique to overcome instabilities associated with deep ResNets.",
+    },
+    {
       title: "Conv Mixer",
       link: "https://ritvik19.medium.com/papers-explained-29-convmixer-f073f0356526",
       date: "January 2022",
