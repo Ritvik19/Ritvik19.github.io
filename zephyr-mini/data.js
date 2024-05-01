6a3165243a7756275d64cf29a8894c6162c70105
Ritvik19 <rastogiritvik99@gmail.com>
2024-05-01 14:28:07 +0530

    Refactor model details table and add Gemma 2B model

--- zephyr-mini/data.js
+++ zephyr-mini/data.js
@@ -15,31 +15,6 @@ let link2icon = {
     "paper": "fas fa-file-pdf",
 }
 
-function create_model_details_table(details) {
-    return table = [
-        ["Model", ...details['titles'].map((title) => title.replace(/_/g, " "))],
-        ["Fine-Tuned Model", ...details['fine_tuned_models'].map((fine_tuned_model) => `<a href="https://huggingface.co/${fine_tuned_model}" target="_blank">${fine_tuned_model}</a>`)],
-        ["Training Approach", ...details['training_approaches'].map((training_approach) => training_approach.replace(/_/g, " "))],
-        ["Base Model", ...details['base_models'].map((base_model) => `<a href="https://huggingface.co/${base_model}" target="_blank">${base_model}</a>`)],
-        ["Tokenizer", ...details['tokenizers'].map((tokenizer) => `<a href="https://huggingface.co/${tokenizer}" target="_blank">${tokenizer}</a>`)],
-        ["Training Config", ...details['training_configs'].map((training_config) => `<a href="${training_config}" target="_blank">View Config</a>`)],
-        ["Model Revision", ...details['revisions']]
-        
-    ];
-}
-
-let model_details = {
-    'TinyLLama': {
-        'titles': ['Zephyr TinyLLama SFT Qlora v0.1'],
-        'training_approaches': ['sft'],
-        'base_models': ['TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T'],
-        'tokenizers': ['Ritvik19/zephyr-tinylamma-sft-qlora'],
-        'fine_tuned_models': ['Ritvik19/zephyr-tinylamma-sft-qlora'],
-        'training_configs': ['https://github.com/Ritvik19/alignment-handbook/blob/main/recipes/zephyr-tinyllama/sft/config_qlora.yaml'],
-        'revisions': ['v0.1']
-    }
-}
-
 let code_snippets = {
     "imports": `
 import torch
@@ -96,8 +71,42 @@ print(response)
 
 }
 
+function create_model_details_table(details) {
+    return table = [
+        ["Model", ...details['titles'].map((title) => title.replace(/_/g, " "))],
+        ["Fine-Tuned Model", ...details['fine_tuned_models'].map((fine_tuned_model) => `<a href="https://huggingface.co/${fine_tuned_model}" target="_blank">${fine_tuned_model}</a>`)],
+        ["Training Approach", ...details['training_approaches'].map((training_approach) => training_approach.replace(/_/g, " "))],
+        ["Base Model", ...details['base_models'].map((base_model) => `<a href="https://huggingface.co/${base_model}" target="_blank">${base_model}</a>`)],
+        ["Tokenizer", ...details['tokenizers'].map((tokenizer) => `<a href="https://huggingface.co/${tokenizer}" target="_blank">${tokenizer}</a>`)],
+        ["Training Config", ...details['training_configs'].map((training_config) => `<a href="${training_config}" target="_blank">View Config</a>`)],
+        ["Model Revision", ...details['revisions']]
+        
+    ];
+}
+
+let model_details = {
+    'TinyLLama': {
+        'titles': ['Zephyr TinyLLama SFT Qlora v0.1'],
+        'training_approaches': ['sft'],
+        'base_models': ['TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T'],
+        'tokenizers': ['Ritvik19/zephyr-tinylamma-sft-qlora'],
+        'fine_tuned_models': ['Ritvik19/zephyr-tinylamma-sft-qlora'],
+        'training_configs': ['https://github.com/Ritvik19/alignment-handbook/blob/main/recipes/zephyr-tinyllama/sft/config_qlora.yaml'],
+        'revisions': ['002c335a34c5176e14c92875084764cee0f7be98']
+    },
+    'Gemma 2B':{
+        'titles': ['Zephyr Gemma 2B SFT Qlora v0.1'],
+        'training_approaches': ['sft'],
+        'base_models': ['google/gemma-2b'],
+        'tokenizers': ['philschmid/gemma-tokenizer-chatml'],
+        'fine_tuned_models': ['Ritvik19/zephyr-2b-gemma-sft-qlora'],
+        'training_configs': ['https://github.com/Ritvik19/alignment-handbook/blob/main/recipes/zephyr-2b-gemma/sft/config_qlora.yaml'],
+        'revisions': ['2a2c01f40945e4104a022f9edd0a764b70bb88a7']
+    }
+}
+
 let project_contents = {
-    "Abstract": [
+    "TL;DR": [
         {"type": "text", "content": "This project centers on the alignment and fine-tuning of small language models (LLMs), drawing on insights from the Zephyr paper by Hugging Face and utilising the resources from the Alignment-Handbook repository. Our objective is to create detailed training recipes for small LLMs and provide aligned checkpoints for open-source distribution."},
         {"type": "text", "content": "We aim to fine-tune smaller LLMs utilizing a combination of tried-and-true methods and novel approaches. Our project embraces the principles of open-source development, ensuring that all training recipes and aligned checkpoints are accessible to the broader community. This approach promotes collaboration, enabling a continuous cycle of improvement and knowledge sharing among researchers and developers."},
         {"type": "text", "content": "Through this work, we offer a flexible framework for aligning small LLMs, designed for easy integration with various applications. The project's overarching goal is to advance the conversation on AI alignment while setting a benchmark for community-driven AI development."},
@@ -108,9 +117,11 @@ let project_contents = {
         {"type": "text", "content": "Prompt Template:"},
         {"type": "code", "content": `<|user|>\\n{prompt}\\n<|assistant>\\n`},
     ],
-    "Results": [
-        {"type": "text", "content": "This section is under development..."},
-    ],
+    "Gemma 2B": [
+        {"type": "table", "columns": ["", ""], "rows": create_model_details_table(model_details['Gemma 2B'])},
+        {"type": "text", "content": "Prompt Template:"},
+        {"type": "code", "content": `<bos><|im_start|>user\\n{prompt}<|im_end|>\\n<|im_start|>assistant\\n`},
+    ],    
     "Usage": [
         {"type": "text", "content": "First, you need to install the required dependencies. You can install them using the following command:"},
         {"type": "code", "content": "!pip install torch transformers peft"},
@@ -124,6 +135,9 @@ let project_contents = {
         {"type": "code", "content": code_snippets['generate']},
         {"type": "text", "content": "Finally, you can generate responses to prompts by calling the above function and using the model specific prompt template:"},
         {"type": "code", "content": code_snippets['prompt']},
+    ],
+    "Results": [
+        {"type": "text", "content": "This section is a work in progress..."},
     ],
 }
 
