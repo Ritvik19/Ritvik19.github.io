67c032d9f738589fb81393984744fdc7c66f35f3
Ritvik19 <rastogiritvik99@gmail.com>
2024-09-08 21:50:02 +0530

    Refactor

--- papers-explained/dataloader.js
+++ papers-explained/dataloader.js
@@ -1,188 +1,122 @@
-function create_section(id, title, contents) {
+function createSection(id, title, contents) {
   return `
     <section id="${id}" class="section">
         <div class="row">
             <div class="col-md-12 left-align">
-                <h2 class="dark-text">
-                    ${title}
-                    <hr />
-                </h2>
+                <h2 class="dark-text">${title}<hr /></h2>
             </div>
         </div>
-
         <div class="row">
-            <div class="col-md-12">
-                ${contents}
-            </div>
+            <div class="col-md-12">${contents}</div>
         </div>
     </section>`;
 }
 
-function create_cards_container(contents) {
+function createCardsContainer(contents) {
+  return `<div class="card-columns">${contents}</div>`;
+}
+
+function createContainerContents(data) {
+  return data.reverse().map(item => createCard(item)).join('');
+}
+
+function createTags(tags) {
+  return `<div class='tags'>${tags.map(tag => `<span class="badge">${tag}</span>`).join('')}</div>`;
+}
+
+function createCard({ title, link, date, description, tags }) {
   return `
-  <div class="card-columns">
-  ${contents}
-  </div>
-  `;
-}
-
-function create_container_contents(data) {
-  console.log(data);
-  let contents = "";
-  for (var i = data.length - 1; i >= 0; i--) {
-    console.log(data[i]);
-    contents += create_cards(
-      data[i].title,
-      data[i].link,
-      data[i].date,
-      data[i].description,
-      data[i].tags
-    );
-  }
-  return contents;
-}
-
-function create_tags(tags) {
-  content = "<div class='tags'>";
-  for (var i = 0; i < tags.length; i++) {
-    content += `<span class="badge">${tags[i]}</span>`;
-  }
-  content += "</div>";
-  return content;
-}
-
-function create_cards(title, link, date, description, tags) {
-  console.log(title, link, date, description, tags);
-  return `
-  <div class="card">
-    <div class="card-body">
-      <h3 class="card-title">${title}</h3>
-      <p class="card-text">${date}<br>${description}</p>
-      ${create_tags(tags)}
-    </div>
-    <div class="card-footer">
-      <a target="_blank" href=${link}>
-        <img src="https://img.shields.io/badge/Read_on-Medium-337ab7?style=flat" alt="Read on Medium">
-      </a>
-    </div>
-  </div>
-  `;
-}
-
-function create_reading_list_section_contents(
-  header,
-  literature_review_data
-) {
-  let contents = `<h4>${header}</h4><div class="card-columns">`;
-  for (var i = 0; i < literature_review_data.length; i++) {
-    contents += `
     <div class="card">
       <div class="card-body">
-        <h3 class="card-title">${literature_review_data[i].title}</h3>
+        <h3 class="card-title">${title}</h3>
+        <p class="card-text">${date}<br>${description}</p>
+        ${createTags(tags)}
       </div>
       <div class="card-footer">
-        <a target="_blank" href=${literature_review_data[i].link}>
+        <a target="_blank" href=${link}>
           <img src="https://img.shields.io/badge/Read_on-Medium-337ab7?style=flat" alt="Read on Medium">
         </a>
       </div>
-    </div>`; 
-  }
-  contents += "</div>";
-  return contents;
+    </div>`;
 }
 
-function create_literature_review_section_contents(
-  header,
-  literature_review_data
-) {
-  let contents = `<h4>${header}</h4><div class="card-columns">`;
-  for (var i = 0; i < literature_review_data.length; i++) {
-    contents += `
+function createSectionWithCards(header, data) {
+  return `
+    <h4>${header}</h4>
+    <div class="card-columns">
+      ${data.map(item => createCard(item)).join('')}
+    </div>`;
+}
+
+function createLiteratureReviewCard({ title, link, papers }) {
+  return `
     <div class="card">
       <div class="card-body">
-        <h3 class="card-title">${literature_review_data[i].title}</h3>
+        <h3 class="card-title">${title}</h3>
       </div>
       <ul class="list-group list-group-flush">
-        ${create_literature_review_list_items(literature_review_data[i].papers)}
+        ${papers.map(paper => `<li class="list-group-item">${paper}</li>`).join('')}
       </ul>
       <div class="card-footer">
-        <a target="_blank" href=${literature_review_data[i].link}>
+        <a target="_blank" href=${link}>
           <img src="https://img.shields.io/badge/Read_on-Medium-337ab7?style=flat" alt="Read on Medium">
         </a>
       </div>
-    </div>`; 
-  }
-  contents += "</div>";
-  return contents;
+    </div>`;
 }
 
-function create_literature_review_list_items(data) {
-  let contents = "";
-  for (var i = 0; i < data.length; i++) {
-    contents += `
-    <li class="list-group-item">
-        ${data[i]}
-    </li>
-    `;
-  }
-  return contents;
+function createLiteratureReviewSection(header, data) {
+  return `
+    <h4>${header}</h4>
+    <div class="card-columns">
+      ${data.map(item => createLiteratureReviewCard(item)).join('')}
+    </div>`;
 }
 
-let container = document.getElementById("container");
-for (var i = 0; i < nav_data.length; i++) {
-  container.innerHTML += create_section(
-    `line_${i + 1}`,
-    nav_data[i],
-    create_cards_container(create_container_contents(papers_data[i]))
+function populateContainer() {
+  let container = document.getElementById("container");
+
+  nav_data.forEach((navItem, index) => {
+    container.innerHTML += createSection(
+      `line_${index + 1}`,
+      navItem,
+      createCardsContainer(createContainerContents(papers_data[index]))
+    );
+  });
+
+  container.innerHTML += createSection(
+    `line_${nav_data.length + 1}`,
+    "Literature Reviews",
+    createLiteratureReviewSection("Literature Reviews", literature_review_data)
+  );
+
+  container.innerHTML += createSection(
+    `line_${nav_data.length + 2}`,
+    "Reading Lists",
+    createSectionWithCards("Reading List", reading_list_data)
   );
 }
 
-container.innerHTML += create_section(
-  `line_${nav_data.length + 1}`,
-  "Literature Reviews",
-  create_literature_review_section_contents(
-    "Literature Reviews",
-    literature_review_data
-  )
-);
-
-container.innerHTML += create_section(
-  `line_${nav_data.length + 2}`,
-  "Reading Lists",
-  create_reading_list_section_contents("Reading List", reading_list_data)
-);
-
-
 function search() {
-  var input, filter, cards;
-  input = document.getElementById('search_input');
-  filter = input.value.toUpperCase();
-  cards = document.getElementsByClassName("card");
-  for (i = 0; i < cards.length; i++) {
-    if (cards[i].textContent.toUpperCase().indexOf(filter) > -1) {
-      cards[i].style.display = "";
-    } else {
-      cards[i].style.display = "none";
-    }
-  }
-  hide_empty_sections();
+  const input = document.getElementById('search_input').value.toUpperCase();
+  const cards = document.getElementsByClassName("card");
+
+  Array.from(cards).forEach(card => {
+    card.style.display = card.textContent.toUpperCase().includes(input) ? "" : "none";
+  });
+
+  hideEmptySections();
 }
 
-function hide_empty_sections() {
-  var sections = document.getElementsByClassName("section");
-  for (i = 0; i < sections.length; i++) {
-    var cards = sections[i].getElementsByClassName("card");
-    var visible = false;
-    for (j = 0; j < cards.length; j++) {
-      if (cards[j].style.display != "none") {
-        visible = true;
-        break;
-      }
-    }
-    if (!visible) {
-      sections[i].style.display = "none";
-    } else {
-      sections[i].style.display = "";
-    }
-  }
+function hideEmptySections() {
+  const sections = document.getElementsByClassName("section");
+
+  Array.from(sections).forEach(section => {
+    const visible = Array.from(section.getElementsByClassName("card"))
+      .some(card => card.style.display !== "none");
+
+    section.style.display = visible ? "" : "none";
+  });
 }
+
+populateContainer();
