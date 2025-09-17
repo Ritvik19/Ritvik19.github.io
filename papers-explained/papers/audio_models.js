70f2bfcd2ef8976b13ae949236204da3d57235a7
Ritvik19 <rastogiritvik99@gmail.com>
2025-09-17 08:59:25 +0530

    added new category

---
+++ papers-explained/papers/audio_models.js
@@ -0,0 +1,10 @@
+const audio_models = [
+    {
+        title: "Pretrained Audio Neural Networks (PANNs)",
+        link: "",
+        date: "December 2019",
+        description:
+            "Addresses the limited research on pretraining systems on large-scale datasets for audio pattern recognition and transferring them to other audio-related tasks. Trained on AudioSet, which contains 1.9 million audio clips with an ontology of 527 sound classes. Explores various convolutional neural network architectures, including a proposed Wavegram-Logmel-CNN that uses both log-mel spectrogram and waveform as input features.",
+        tags: ["Audio Models", "Transfer Learning"],
+    }
+]
